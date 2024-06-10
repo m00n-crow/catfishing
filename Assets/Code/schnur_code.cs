b078b3589595e8070ae9af6657355c13985a457c
Patrick <56729093+Warmund@users.noreply.github.com>
2024-06-11 01:01:26 +0200

    small updates

--- Assets/Code/schnur_code.cs
+++ Assets/Code/schnur_code.cs
@@ -2,30 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-//für visuelle Darstellung der schnur
-
+// für visuelle Darstellung der Schnur
 public class Schnur : MonoBehaviour
 {
-    public Transform Angel;
     public Transform Haken;
 
     private LineRenderer LineRenderer;
-   //  public Color lineColor = Color.white;
+    // public Color lineColor = Color.white;
 
     void Start()
     {
         LineRenderer = GetComponent<LineRenderer>();
-        LineRenderer.positionCount = 2; //start und end- punkt
+        if (LineRenderer == null)
+        {
+            Debug.LogError("LineRenderer nicht gefunden!");
+            return;
+        }
+
+        if (Haken == null)
+        {
+            Debug.LogError("Haken Transform ist nicht zugewiesen!");
+            return;
+        }
+
+        LineRenderer.positionCount = 2; // start und end- punkt
     }
 
     void Update()
     {
+        if (Haken == null)
+        {
+            Debug.LogError("Haken Transform ist nicht zugewiesen!");
+            return;
+        }
+
         Vector3 startPosition = new Vector3(-3.1f, -0.1f, 0f);
         Vector3 endPosition = new Vector3(Haken.position.x, Haken.position.y, 0);
 
         LineRenderer.SetPosition(0, startPosition);
         LineRenderer.SetPosition(1, endPosition);
-
     }
 }
