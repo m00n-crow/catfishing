ad3aae3efde7d8c8592b5f4b10ce4fd4214021af
wirbelstrm <ocramw@hotmail.de>
2024-06-18 18:53:25 +0200

    Inventar hat fische und zähler, nur noch keinen zurück button T^T

--- Assets/Code/ShopManagerScript.cs
+++ Assets/Code/ShopManagerScript.cs
@@ -1,44 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Für normale UI-Text
+using TMPro; // Für TextMeshPro
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
+using System.Collections.Generic;
 
-
-
-//managed quality und quantity im shop..
 public class ShopManagerScript : MonoBehaviour
 {
-   public int[,] shopItems = new int[5, 5];
-   public float coins;
-   public Text CoinsTXT;
+    public int[,] shopItems = new int[5, 5];
+    public float coins;
+    public Text CoinsTXT;
+
+    public Image[] fishImages; // Array der Fisch-Icons für das Inventar
+    public TextMeshProUGUI[] fishCounters; // Array der Textfelder für die Counter
+    public GameObject fishDetailsPanel; // Panel für Details
+    public Text fishDescription; // Textfeld für die Beschreibung
+    public Image hiddenFishSlot; // Slot für den versteckten Fisch
+
+    private Dictionary<int, int> fishInventory = new Dictionary<int, int>(); // Inventar
+
     void Start()
     {
-
         CoinsTXT.text = "Coins: " + coins.ToString();
 
-        shopItems[1,1] = 1;
-        shopItems[1,2] = 2;
-        shopItems[1,3] = 3;
-        shopItems[1,4] = 4;
+        shopItems[1, 1] = 1;
+        shopItems[1, 2] = 2;
+        shopItems[1, 3] = 3;
+        shopItems[1, 4] = 4;
 
-        //price
+        // Preise
+        shopItems[2, 1] = 10;
+        shopItems[2, 2] = 15;
+        shopItems[2, 3] = 20;
+        shopItems[2, 4] = 25;
 
-        shopItems[1,1] = 10;
-        shopItems[1,2] = 15;
-        shopItems[1,3] = 20;
-        shopItems[1,4] = 25;
+        // Menge
+        shopItems[3, 1] = 1;
+        shopItems[3, 2] = 2;
+        shopItems[3, 3] = 3;
+        shopItems[3, 4] = 4;
 
-        //quantity
+        // Set hidden fish slot inactive initially
+        hiddenFishSlot.gameObject.SetActive(false);
 
-        shopItems[3,1] = 1;
-        shopItems[3,2] = 2;
-        shopItems[3,3] = 3;
-        shopItems[3,4] = 4;
-
+        // Initialize fish inventory counters
+        for (int i = 0; i < fishImages.Length; i++)
+        {
+            fishInventory[i] = 0;
+            fishImages[i].gameObject.SetActive(false); // Alle Fisch-Images initial deaktivieren
+        }
     }
 
-   
     public void Buy()
     {
         GameObject ButtonRef = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
@@ -50,6 +61,33 @@ public class ShopManagerScript : MonoBehaviour
             CoinsTXT.text = "Coins: " + coins.ToString();
             ButtonRef.GetComponent<ButtonInfo>().QuantityTxt.text = shopItems[3, ButtonRef.GetComponent<ButtonInfo>().ItemID].ToString();
 
+            // Update inventory
+            int fishID = ButtonRef.GetComponent<ButtonInfo>().ItemID;
+            UpdateInventory(fishID);
         }
+    }
+
+    private void UpdateInventory(int fishID)
+    {
+        fishInventory[fishID]++;
+        fishImages[fishID].gameObject.SetActive(true); // Fisch-Image aktivieren
+        fishCounters[fishID].text = fishInventory[fishID].ToString();
+    }
+
+    public void ShowFishDetails(int fishID)
+    {
+        fishDetailsPanel.SetActive(true);
+        fishDescription.text = "Description for fish " + fishID;
+    }
+
+    public void HideFishDetails()
+    {
+        fishDetailsPanel.SetActive(false);
+    }
+
+    public void CatchSpecialFish(int fishID)
+    {
+        hiddenFishSlot.gameObject.SetActive(true);
+        UpdateInventory(fishID);
     }
 }
