56e21f5cf1b81908a17087758ac089cd6061e22e
Patrick <56729093+Warmund@users.noreply.github.com>
2024-06-21 16:52:30 +0200

    speeded background for better orientation

--- Assets/Code/notebook_button_controller.cs
+++ Assets/Code/notebook_button_controller.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class ShopController : MonoBehaviour
 {
     public GameObject shopCanvas;
+    bool opened = false;
 
     // Funktion zum Öffnen des Shops
     public void OpenShop()
     {
-        shopCanvas.SetActive(true);
+        if (!opened)
+        {
+            shopCanvas.SetActive(true);
+            opened = true; 
+        }
+        else
+        {
+            shopCanvas.SetActive(false);
+            opened = false;
+
+        }    
     }
 
     // Funktion zum Schließen des Shops
